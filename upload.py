642050dfaa8bbd1f11acead63f8cc24181a1a2b2
dragon0486 <869071540@qq.com>
2020-04-03 20:22:13 +0800

    update voc upload tool

--- upload.py
+++ upload.py
@@ -117,7 +117,6 @@ def upload_model_predict_result_from_coco(coco_file_path,project_id,dataset_id,v
         upload_model_predict_result("images",project_id,dataset_id,verbose)
 
 def upload_model_predict_result_from_voc(voc_anno_path,voc_image_path,project_id,dataset_id,verbose=True,args=None):
-    print(args)
     utils.check_path_exist(voc_anno_path)
     out_json_path = os.path.join("./", "template_for_convert")
     utils.remove_directiry(out_json_path)
@@ -185,8 +184,7 @@ def run_command(args, command, nargs, parser):
             parser.print_help()
             print("upload_model_predict_result_from_voc [voc_anno_path] [voc_image_path] [project_id] [dataset_id]")
         else:
-            print(args)
-            upload_model_predict_result_from_voc(nargs[0], nargs[1],nargs[2],args.verbose,args)
+            upload_model_predict_result_from_voc(nargs[0], nargs[1],nargs[2],nargs[3],args.verbose,args)
     elif command == "upload_model_predict_result_from_ocr":
         if len(nargs) != 3:
             parser.print_help()
